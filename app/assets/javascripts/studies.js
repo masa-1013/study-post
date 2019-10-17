fbb62a73ca319a9b53750360b2ca19b95abfd76c
春冨雅大 <masahiro@masanoMacBook-Pro.local>
2019-10-17 22:11:53 +0900

    modify getting wrong id

--- app/assets/javascripts/studies.js
+++ app/assets/javascripts/studies.js
@@ -2,12 +2,12 @@ $(document).on("turbolinks:load", function() {
   $("form").on("submit", function() {
     if ($("#comment_content").val() === "") {
       alert("コメントを入力して下さい")
-      return false;
+      return false
     }
   })
   $(".study .list-group-item").on("click", function(e) {
     if (e.target.id !== "user_link") {
-      var id = $("#study_id").val()
+      var id = $(this).parent().find("#study_id").val()
       location.href = "/studies/" + encodeURIComponent(id)
     } 
   })
